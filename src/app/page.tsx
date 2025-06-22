8f68ff8f2278e200b9ec0212bad11e9007d1f784
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-06-22 18:03:10 +0300

    style(page): improve layout and spacing for Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,18 @@ import VoteCards from '@/components/vote/VoteCards';
 
 export default function Home() {
   return (
-    <div>
-      <div className="mx-auto flex min-h-screen max-w-xl flex-col items-center justify-center">
-        <VoteCards />
-        <UserVotesTable />
+    <div className="mx-auto flex min-h-screen max-w-xl flex-col items-center justify-center space-y-4">
+      <div className="space-y-2 text-center">
+        <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          Estimate the Effort
+        </h2>
+        <p className="text-muted-foreground max-w-md text-center text-sm">
+          Pick a card that reflects how complex or time-consuming you think this
+          task is.
+        </p>
       </div>
+      <VoteCards />
+      <UserVotesTable />
     </div>
   );
 }
