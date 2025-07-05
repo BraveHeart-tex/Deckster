b6f8551505af7ce36a9ae7ff5e7500689e5ce6f4
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-07-05 17:17:01 +0300

    feat(FE-40): check if the user has access to the room

--- constants.ts
+++ constants.ts
@@ -1,1 +1,2 @@
 export const CONVEX_JWT_TEMPLATE_NAME = 'convex';
+export const CONVEX_ID_LENGTH = 32;
--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,9 +1,38 @@
+import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
 import UserVotesTable from '@/src/components/vote/UserVotesTable';
 import VoteCards from '@/src/components/vote/VoteCards';
 import VoteControls from '@/src/components/vote/VoteControls';
+import { ROUTES } from '@/src/constants/routes';
+import { getConvexJwtToken } from '@/src/helpers/auth';
+import { isValidConvexId } from '@/src/lib/utils';
+import { auth } from '@clerk/nextjs/server';
+import { fetchQuery } from 'convex/nextjs';
+import { redirect } from 'next/navigation';
 
 const RoomPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { isAuthenticated } = await auth();
+
+  if (!isAuthenticated) {
+    redirect(ROUTES.AUTH);
+  }
+
+  if (!isValidConvexId((await params).id)) {
+    redirect(ROUTES.HOME);
+  }
+
+  const userHasAccessToRoom = await fetchQuery(
+    api.rooms.getUserHasAccessToRoom,
+    { roomId: (await params).id as Id<'rooms'> },
+    {
+      token: await getConvexJwtToken(),
+    }
+  );
+
+  if (!userHasAccessToRoom) {
+    redirect(ROUTES.HOME);
+  }
+
   return (
     <div className="mx-auto flex min-h-screen max-w-screen-xl flex-col items-center justify-center space-y-4">
       <div className="space-y-2 text-center">
