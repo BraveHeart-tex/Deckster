2bfd375ccb537ae2e1d32ca5dccc25f9d2d368ba
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-07-28 09:26:25 +0300

    fix: fix share room dialog visibility logic so its only visible on room page

--- src/components/room/ShareRoomDialogWrapper.tsx
+++ src/components/room/ShareRoomDialogWrapper.tsx
@@ -1,19 +1,21 @@
 'use client';
+import { usePathname } from 'next/navigation';
 import ShareRoomDialog from '@/src/components/room/ShareRoomDialog';
 import { Skeleton } from '@/src/components/ui/skeleton';
 import { useStateBus } from '@/src/hooks/useStateBus';
+import { ROUTES } from '@/src/lib/routes';
 
 const ShareRoomDialogWrapper = () => {
   const [isJoining] = useStateBus('isJoiningRoom');
+  const pathname = usePathname();
 
-  if (isJoining) {
+  if (isJoining && pathname.includes(ROUTES.ROOM('')))
     return (
       <div className='flex items-center gap-2'>
         <Skeleton className='size-[1.5rem] rounded-md' />
         <Skeleton className='h-[1.5rem] w-20 rounded-md' />
       </div>
     );
-  }
 
   return <ShareRoomDialog />;
 };
